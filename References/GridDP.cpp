1755f77e982d73c4ac227c8b255f95c5f8e04a45
Akshar Panchani <aksharpanchani29@gmail.com>
2024-05-28 23:17:48 +0530

    Medium DP

--- References/GridDP.cpp
+++ References/GridDP.cpp
@@ -48,26 +48,26 @@ int main() {
 // #include <iostream>
 // #include <vector>
 
-// int countWays(int m, int n) {
-//     std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+int countWays(int m, int n) {
+    std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
 
-//     for (int i = 0; i < m; i++) {
-//         for (int j = 0; j < n; j++) {
-//             if (i == 0 && j == 0) {
-//                 dp[i][j] = 1;
-//             } else {
-//                 if (i > 0) {
-//                     dp[i][j] += dp[i - 1][j];
-//                 }
-//                 if (j > 0) {
-//                     dp[i][j] += dp[i][j - 1];
-//                 }
-//             }
-//         }
-//     }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+            } else {
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+    }
 
-//     return dp[m - 1][n - 1];
-// }
+    return dp[m - 1][n - 1];
+ }
 
 // int main() {
 //     int m = 3;
