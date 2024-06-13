268c66763ea19d231ffde218a5eee32803a1c9d2
Akshar Panchani <aksharpanchani29@gmail.com>
2024-06-13 17:31:22 +0530

    Medium

---
+++ SlidingWindow/NoOfSubsringWithAll3Character.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        vector<int> lastseen(3,-1);
+        int count=0;
+        for(int i=0;i<n;i++){
+            lastseen[s[i]-'a']=i;
+            count=count +1+min({lastseen[0],lastseen[1],lastseen[2]});
+        }
+        return count;
+    }
+};
