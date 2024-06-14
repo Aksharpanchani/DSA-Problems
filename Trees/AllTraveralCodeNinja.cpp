e6521a7ac8ae6399dd5e6b22e911b0103cca656b
Akshar Panchani <aksharpanchani29@gmail.com>
2024-06-14 15:17:32 +0530

    Solved

---
+++ Trees/AllTraveralCodeNinja.cpp
@@ -0,0 +1,56 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<vector<int>> getTreeTraversal(TreeNode *root){
+    // Write your code here.
+    vector<vector<int>> res;
+         vector<int> inOrder;
+            vector<int> preOrder;
+            vector<int> postOrder;
+            stack<TreeNode*> s;
+            TreeNode* curr = root;
+            while(curr!=NULL || !s.empty()){
+                while(curr!=NULL){
+                    s.push(curr);
+                    curr = curr->left;
+                }
+                curr = s.top();
+                s.pop();
+                inOrder.push_back(curr->data);
+                curr = curr->right;
+            }
+            res.push_back(inOrder);
+            s.push(root);
+            while(!s.empty()){
+                TreeNode* curr = s.top();
+                s.pop();
+                preOrder.push_back(curr->data);
+                if(curr->right!=NULL) s.push(curr->right);
+                if(curr->left!=NULL) s.push(curr->left);
+            }
+            res.push_back(preOrder);
+            stack<TreeNode*> s1;
+            s.push(root);
+            while(!s.empty()){
+                TreeNode* curr = s.top();
+                s.pop();
+                s1.push(curr);
+                if(curr->left!=NULL) s.push(curr->left);
+                if(curr->right!=NULL) s.push(curr->right);
+            }
+            while(!s1.empty()){
+                postOrder.push_back(s1.top()->data);
+                s1.pop();
+            }
+            res.push_back(postOrder);
+            return res;
+}
