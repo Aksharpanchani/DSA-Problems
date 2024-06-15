d17529027db3056c7f4d6c141916a0dc178fa957
Akshar Panchani <aksharpanchani29@gmail.com>
2024-06-15 18:41:07 +0530

    Updated Easy

--- Trees/LeetCodePostorder.cpp
+++ Trees/LeetCodePostorder.cpp
@@ -1,3 +1,40 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> postOrder;
+//          if (!root) return postOrder;
+
+//         stack<TreeNode*> s;
+//         TreeNode* curr = root;
+//         stack<TreeNode*> s1;
+//             s.push(root);
+//             while(!s.empty()){
+//                 TreeNode* curr = s.top();
+//                 s.pop();
+//                 s1.push(curr);
+//                 if(curr->left!=NULL) s.push(curr->left);
+//                 if(curr->right!=NULL) s.push(curr->right);
+//             }
+//             while(!s1.empty()){
+//                 postOrder.push_back(s1.top()->val);
+//                 s1.pop();
+//             }
+//             return postOrder;
+//     }
+// };
+
+//---------------------------------Easy -------------------------------------
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -11,25 +48,18 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postOrder;
-         if (!root) return postOrder;
 
-        stack<TreeNode*> s;
-        TreeNode* curr = root;
-        stack<TreeNode*> s1;
-            s.push(root);
-            while(!s.empty()){
-                TreeNode* curr = s.top();
-                s.pop();
-                s1.push(curr);
-                if(curr->left!=NULL) s.push(curr->left);
-                if(curr->right!=NULL) s.push(curr->right);
-            }
-            while(!s1.empty()){
-                postOrder.push_back(s1.top()->val);
-                s1.pop();
-            }
-            return postOrder;
+    void postorder(TreeNode* root, vector<int> &post){
+    if(root==NULL) return;
+
+    postorder(root->left,post);
+    
+    postorder(root->right,post);
+    post.push_back(root->val);
+}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> post;
+         postorder(root,post);
+            return post;
     }
 };
