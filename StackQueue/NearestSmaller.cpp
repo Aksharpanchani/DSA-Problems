e3689354eb88a11473f3dd18050635b2e3efec5b
Akshar Panchani <aksharpanchani29@gmail.com>
2024-07-05 19:54:27 +0530

    Medium Easy

---
+++ StackQueue/NearestSmaller.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    vector<int> ans(A.size(),-1);
+    stack<int> st;
+    
+    for(int i=0;i<A.size();i++){
+        while(!st.empty() && st.top()>=A[i]){
+            st.pop();
+        }
+        ans[i] = st.empty() ? -1 : st.top();
+        st.push(A[i]);
+    }
+    return ans;
+}
