26f36108dfa403a41c5e4024e540167d4efe8d75
Akshar Panchani <aksharpanchani29@gmail.com>
2024-09-20 12:50:21 +0530

    Daily problem

--- Strings/ShortestPalindrone.cpp
+++ Strings/ShortestPalindrone.cpp
@@ -25,3 +25,33 @@ public:
         return str+s;
     }
 };
+
+//Daily challenge
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string str = s;
+        reverse(str.begin(), str.end());
+        str = s + "$" + str;
+        int n = str.size();
+        vector<int> toaddstring(n);
+        int i=1, j=0;
+        while(i<n){
+            if(str[i]==str[j]){
+                j++;
+                toaddstring[i] = j;
+                i++;
+            }else{
+                if(j!=0)
+                    j = toaddstring[j-1];
+                else
+                    i++;
+            }
+        }
+        i = toaddstring[n-1];
+        str = s.substr(i, s.size());
+        reverse(str.begin(), str.end());
+        return str+s;
+    }
+};
