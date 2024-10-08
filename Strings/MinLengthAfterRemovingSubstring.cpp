bfc5582d8e941d7ffe6aa664b6c0aee80acadb8e
Akshar Panchani <aksharpanchani29@gmail.com>
2024-10-08 15:28:08 +0530

    Easy

--- Strings/MinLengthAfterRemovingSubstring.cpp
+++ Strings/MinLengthAfterRemovingSubstring.cpp
@@ -18,3 +18,23 @@ public:
 
     }     
 };
+
+// Rewrite the code but the for loop should be in the fashion as for(int i=0; i<n; i++) where n is the size of the string
+
+class solution{
+    public:
+    int minLength(string s){
+        stack<char> st;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            if(!st.empty() && st.top() == 'A' && s[i] == 'B'){
+                st.pop();
+            } else if(!st.empty() && st.top() == 'C' && s[i] == 'D'){
+                st.pop();
+            } else {
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
